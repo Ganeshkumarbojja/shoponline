20f8c53a9d647b66fec994fc2297602417cf8e8e
Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2024-03-11 17:38:40 +0530

    added not foundpage

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,3 +1,42 @@
-const NotFound = () => <div>Not Found</div>;
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import Header from "../Header";
+
+import {
+  NotFoundContainer,
+  NotFoundCard,
+  NotFoundImage,
+  NotFoundTitle,
+  NotFoundDescription,
+  GotoHomeBtn,
+} from "./styledComponents";
+
+import AppContext from "../../context/appContext";
+
+const NotFound = () => {
+  const context = useContext(AppContext);
+  const { isdark } = context;
+  return (
+    <>
+      <Header />
+      <NotFoundContainer isdark={isdark}>
+        <NotFoundCard isdark={isdark}>
+          <NotFoundImage
+            src="https://img.freepik.com/free-vector/404-error-with-person-looking-concept-illustration_114360-7982.jpg?t=st=1710151232~exp=1710154832~hmac=429353f25fbd94531d72a1615318a8bda1d82f6385dd679d5e63ab253c208062&w=740"
+            alt="not found"
+          />
+          <NotFoundTitle isdark={isdark}>Page Not Found</NotFoundTitle>
+          <NotFoundDescription isdark={isdark}>
+            We are sorry, the page you are looking for cannot be found.
+          </NotFoundDescription>
+          <Link to="/">
+            <GotoHomeBtn>Go to Home</GotoHomeBtn>
+          </Link>
+        </NotFoundCard>
+      </NotFoundContainer>
+    </>
+  );
+};
 
 export default NotFound;
--- src/components/NotFound/styledComponents.js
+++ src/components/NotFound/styledComponents.js
@@ -1,58 +1,61 @@
 import styled from "styled-components";
 
-export const HomeContainer = styled.div`
+export const NotFoundContainer = styled.div`
   min-height: 90vh;
   display: flex;
   align-items: center;
   justify-content: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
   background-color: ${(props) => (props.isdark ? "#171616" : "#ffffff")};
 `;
 
-export const HomeCard = styled.div`
+export const NotFoundCard = styled.div`
   box-shadow: ${(props) =>
     props.isdark ? "none" : "0px 4px 16px 0px #bfbfbf"};
-  width: 90%;
   max-width: 1200px;
+  width: 50%;
   padding: 50px;
   border-radius: 20px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: ${(props) => (props.isdark ? "#000000" : "#ffffff")};
   @media screen and (max-width: 767px) {
-    flex-direction: column;
+    width: 80%;
     padding: 20px;
   }
 `;
 
-export const HomeImage = styled.img`
-  width: 40%;
-  border-radius: 20px;
-  margin-left: 30px;
+export const NotFoundImage = styled.img`
+  width: 50%;
+  border-radius: 10px;
   @media screen and (max-width: 767px) {
-    order: -1;
     width: 100%;
-    align-self: center;
-    margin-left: 0px;
   }
 `;
 
-export const HomeTitle = styled.h1`
-  font-size: 45px;
+export const NotFoundTitle = styled.h1`
+  font-size: 40px;
   color: ${(props) => (props.isdark ? "#ffffff" : "#000000")};
+  margin-bottom: 0px;
+  text-align: center;
   @media screen and (max-width: 767px) {
     font-size: 30px;
   }
 `;
 
-export const HomeDescription = styled.p`
+export const NotFoundDescription = styled.p`
   font-size: 18px;
   line-height: 26px;
+  text-align: center;
   color: ${(props) => (props.isdark ? "#e3e1e1" : "#787777")};
   @media screen and (max-width: 767px) {
     font-size: 16px;
   }
 `;
 
-export const ShopNowBtn = styled.button`
+export const GotoHomeBtn = styled.button`
   background-color: midnightblue;
   cursor: pointer;
   color: #ffffff;
