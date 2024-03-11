8fd986ca63017c4c5bb3a6f9072db2d41c65cbdf
Ganesh Kumar <bojjaganeshkumar1@gmail.com>
2024-03-11 18:28:48 +0530

    added products header with filter functionality

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,39 +4,88 @@ import { useContext } from "react";
 import ProductItem from "../ProductItem";
 import Loader from "../Loader";
 
-import { ProductsContainer, ProductsList } from "./styledComponents";
+import {
+  ProductsContainer,
+  ProductsList,
+  ProductsHeader,
+  FilterLabel,
+  ProductsTitle,
+  CategoriesList,
+} from "./styledComponents";
 import AppContext from "../../context/appContext";
 import Header from "../Header";
 
 const Products = () => {
   const [productsList, setProductsList] = useState([]);
+  const [categoriesList, setCategoriesList] = useState([]);
+  const [activeCategory, setActiveCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const context = useContext(AppContext);
   const { isdark } = context;
   const getProducts = async () => {
-    const url = "https://fakestoreapi.com/products?sort=asc";
+    let categoriesPath = "";
+    if (activeCategory !== "") {
+      categoriesPath = `category/${activeCategory}`;
+    }
+    const productsUrl = `https://fakestoreapi.com/products/${categoriesPath}?sort=asc`;
     setIsLoading(true);
-    const response = await fetch(url);
-    const data = await response.json();
-    setProductsList(data);
+    const productsResponse = await fetch(productsUrl);
+    const productsData = await productsResponse.json();
+    setProductsList(productsData);
+    const categoriesUrl = "https://fakestoreapi.com/products/categories";
+    const categoiesResponse = await fetch(categoriesUrl);
+    const categoiesData = await categoiesResponse.json();
+    setCategoriesList(categoiesData);
     setIsLoading(false);
   };
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [activeCategory]);
+
+  const onCategoryChange = (e) => {
+    if (e.target.value === "all") {
+      setActiveCategory("");
+    } else {
+      setActiveCategory(e.target.value);
+    }
+  };
+
+  const renderProductsBody = () => (
+    <>
+      <ProductsHeader>
+        <ProductsTitle isdark={isdark}>Products</ProductsTitle>
+        <div>
+          <FilterLabel isdark={isdark} htmlFor="categories">
+            Filter
+          </FilterLabel>
+          <CategoriesList
+            onChange={onCategoryChange}
+            value={activeCategory}
+            id="categories"
+          >
+            <option value="all" key="all">
+              All
+            </option>
+            {categoriesList.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </CategoriesList>
+        </div>
+      </ProductsHeader>
+      <ProductsList>
+        {productsList.map((product) => (
+          <ProductItem key={product.id} productDetails={product} />
+        ))}
+      </ProductsList>
+    </>
+  );
   return (
     <>
       <Header />
       <ProductsContainer isdark={isdark}>
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <ProductsList>
-            {productsList.map((product) => (
-              <ProductItem key={product.id} productDetails={product} />
-            ))}
-          </ProductsList>
-        )}
+        {isLoading ? <Loader /> : renderProductsBody()}
       </ProductsContainer>
     </>
   );
--- src/components/Products/styledComponents.js
+++ src/components/Products/styledComponents.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ProductsContainer = styled.div`
   min-height: 90vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   background-color: ${(props) => (props.isdark ? "#171616" : "#ffffff")};
@@ -18,4 +19,37 @@ export const ProductsList = styled.ul`
   @media screen and (max-width: 576px) {
     justify-content: center;
   }
+`;
+
+export const ProductsHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  align-self: flex-start;
+  padding-left: 60px;
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 50px;
+  }
+`;
+
+export const FilterLabel = styled.label`
+  font-size: 15px;
+  font-weight: 500;
+  color: ${(props) => (props.isdark ? "#ffffff" : "#000000")};
+  margin-right: 10px;
+`;
+
+export const ProductsTitle = styled.h1`
+  font-size: 35px;
+  font-weight: 500;
+  color: ${(props) => (props.isdark ? "#ffffff" : "#000000")};
+`;
+
+export const CategoriesList = styled.select`
+  outline: none;
+  height: 30px;
 `;
